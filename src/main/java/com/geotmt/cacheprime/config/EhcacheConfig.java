9c4257d52f51c9d8cf5c9757ca11dc3cba1a24b8
zhangjiandong <zhangjiandong@geotmt.com>
2019-11-06 11:23:59 +0800

    ehcache 测试ttl与tti的关系 后者设置的会覆盖前者

--- src/main/java/com/geotmt/cacheprime/config/EhcacheConfig.java
+++ src/main/java/com/geotmt/cacheprime/config/EhcacheConfig.java
@@ -9,6 +9,7 @@ import org.ehcache.config.units.EntryUnit;
 import org.ehcache.config.units.MemoryUnit;
 import org.ehcache.expiry.Duration;
 import org.ehcache.expiry.Expirations;
+import org.ehcache.impl.internal.store.heap.OnHeapStore;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,37 +25,28 @@ public class EhcacheConfig {
     @Bean(name="ehcacheManager")
     public CacheManager ehcacheManager() {
 
-        // 配置默认缓存属性
-        CacheConfiguration<String, String> defaultCacheConfig = CacheConfigurationBuilder.newCacheConfigurationBuilder(
-                // 缓存数据K和V的数值类型
-                // 在ehcache3.3中必须指定缓存键值类型,如果使用中类型与配置的不同,会报类转换异常
-                String.class, String.class,
-                ResourcePoolsBuilder
-                        .newResourcePoolsBuilder()
-                        .heap(50L, EntryUnit.ENTRIES)  //设置缓存堆容纳元素个数(JVM内存空间)超出个数后会存到offheap中
-                        .offheap(100L, MemoryUnit.MB)    //设置堆外储存大小(内存存储) 超出offheap的大小会淘汰规则被淘汰
-                        .disk(500L, MemoryUnit.MB, false))// 配置磁盘持久化储存(硬盘存储)用来持久化到磁盘,这里设置为false不启用
-                        .withExpiry(Expirations.timeToLiveExpiration(Duration.of(30L, TimeUnit.SECONDS)))//设置缓存过期时间
-                        .withExpiry(Expirations.timeToIdleExpiration(Duration.of(60L, TimeUnit.SECONDS))  //设置被访问后过期时间(同时设置和TTL和TTI之后会被覆盖,这里TTI生效,之前版本xml配置后是两个配置了都会生效) // 缓存淘汰策略 默认策略是LRU（最近最少使用）。你可以设置为FIFO（先进先出）或是LFU（较少使用）。
-                        ).build();
+        ResourcePoolsBuilder  poolsBuilder = ResourcePoolsBuilder
+                .newResourcePoolsBuilder()
+                .heap(5000L, EntryUnit.ENTRIES)              //设置缓存堆容纳元素个数(JVM内存空间)超出个数后会存到offheap中
+                .offheap(30L, MemoryUnit.MB)              //设置堆外储存大小(内存存储) 超出offheap的大小会淘汰规则被淘汰
+                .disk(100L, MemoryUnit.MB, false); //// 配置磁盘持久化储存(硬盘存储)用来持久化到磁盘,这里设置为false不启用
 
-        CacheConfiguration<String, String> userCacheConfig = CacheConfigurationBuilder.newCacheConfigurationBuilder(
-                // 缓存数据K和V的数值类型
-                // 在ehcache3.3中必须指定缓存键值类型,如果使用中类型与配置的不同,会报类转换异常
-                String.class, String.class,
-                ResourcePoolsBuilder
-                        .newResourcePoolsBuilder()
-                        .heap(3, EntryUnit.ENTRIES)  //设置缓存堆容纳元素个数(JVM内存空间)超出个数后会存到offheap中
-                        .offheap(6, MemoryUnit.MB)    //设置堆外储存大小(内存存储) 超出offheap的大小会淘汰规则被淘汰
-                        .disk(10L, MemoryUnit.MB, false))// 配置磁盘持久化储存(硬盘存储)用来持久化到磁盘,这里设置为false不启用
-                .withExpiry(Expirations.timeToLiveExpiration(Duration.of(1L, TimeUnit.SECONDS)))//设置缓存过期时间
-                .withExpiry(Expirations.timeToIdleExpiration(Duration.of(1L, TimeUnit.SECONDS)))  //设置被访问后过期时间(同时设置和TTL和TTI之后会被覆盖,这里TTI生效,之前版本xml配置后是两个配置了都会生效) // 缓存淘汰策略 默认策略是LRU（最近最少使用）。你可以设置为FIFO（先进先出）或是LFU（较少使用）。
+        // 配置默认缓存属性
+        CacheConfiguration<String, String> defaultCacheConfig = CacheConfigurationBuilder
+                .newCacheConfigurationBuilder(String.class, String.class, poolsBuilder)                   // 缓存数据K和V的数值类型
+                .withExpiry(Expirations.timeToLiveExpiration(Duration.of(30L, TimeUnit.SECONDS)))  //设置缓存过期时间
+                .withExpiry(Expirations.timeToIdleExpiration(Duration.of(60L, TimeUnit.SECONDS)))  //设置被访问后过期时间(同时设置和TTL和TTI之后会被覆盖,这里TTI生效,之前版本xml配置后是两个配置了都会生效)
                 .build();
 
+        CacheConfiguration<String, String> userCacheConfig = CacheConfigurationBuilder.newCacheConfigurationBuilder(
+                String.class, String.class, ResourcePoolsBuilder.newResourcePoolsBuilder().heap(300, EntryUnit.ENTRIES).offheap(30, MemoryUnit.MB).disk(60L, MemoryUnit.MB, false))
+                .withExpiry(Expirations.timeToIdleExpiration(Duration.of(1L, TimeUnit.SECONDS)))
+                .withExpiry(Expirations.timeToLiveExpiration(Duration.of(1L, TimeUnit.SECONDS)))
+                .build();
 
         // CacheManager管理缓存
         CacheManager cacheManager = CacheManagerBuilder.newCacheManagerBuilder()
-                .with(CacheManagerBuilder.persistence("f:/ehcacheData"))// 硬盘持久化地址
+                .with(CacheManagerBuilder.persistence("e:/ehcacheData"))// 硬盘持久化地址
                 .withCache("defaultCache", defaultCacheConfig) //// 设置一个默认缓存配置
                 .withCache("userCache",userCacheConfig)
                 .build(true);//创建之后立即初始化
@@ -66,7 +58,7 @@ public class EhcacheConfig {
 //        2、timeToIdleSeconds的定义是：在创建时间和最近访问时间中取出离现在最近的时间作为基准计算的超时时长；
 //        3、如果仅设置了timeToLiveSeconds，则该对象的超时时间=创建时间+timeToLiveSeconds，假设为A；
 //        4、如果没设置timeToLiveSeconds，则该对象的超时时间=min(创建时间，最近访问时间)+timeToIdleSeconds，假设为B；
-//        5、如果两者都设置了，则取出A、B最少的值，即min(A,B)，表示只要有一个超时成立即算超时。
+//        5、如果两者都设置了，则取出A、B 谁后设置的谁生效，也就是ttl与tti是存在覆盖的问题的，后面的设置会覆盖前面的设置。
     }
 
 
--- src/test/java/com/geotmt/cacheprime/EhcacheTest.java
+++ src/test/java/com/geotmt/cacheprime/EhcacheTest.java
@@ -4,11 +4,20 @@ package com.geotmt.cacheprime;
 import lombok.extern.log4j.Log4j2;
 import org.ehcache.Cache;
 import org.ehcache.CacheManager;
+import org.ehcache.config.CacheConfiguration;
+import org.ehcache.config.builders.CacheConfigurationBuilder;
+import org.ehcache.config.builders.ResourcePoolsBuilder;
+import org.ehcache.config.units.EntryUnit;
+import org.ehcache.config.units.MemoryUnit;
+import org.ehcache.expiry.Duration;
+import org.ehcache.expiry.Expirations;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.TimeUnit;
 
 @Log4j2
 @RunWith(SpringRunner.class)
@@ -18,40 +27,59 @@ public class EhcacheTest {
     @Autowired
     private CacheManager ehcacheManager;
 
-
-    /**
-     * key:测试
-     * key2:测试2
-     * key:测试
-     * key:null
-     * @throws InterruptedException
-     */
+    // userCache中的ttl是后设置的因此不管之前是不是一直访问key,到一分钟后key都会失效
     @Test
-    public void ehcache() throws InterruptedException {
-
+    public void ehcacheTestTTL() throws InterruptedException {
 
         Cache<String, String> mineCache = ehcacheManager.getCache("userCache", String.class, String.class);
         StringBuilder strTemp = new StringBuilder("测试");
         mineCache.put("key", strTemp.toString());
-        System.out.println("key:" + mineCache.get("key"));
 
-        strTemp = new StringBuilder("测试2");
-        mineCache.put("key2", strTemp.toString());
-        System.out.println("key2:" + mineCache.get("key2"));
+        int count = 0;
+        while(true){
+            Thread.sleep(50);
+            String keyValue = mineCache.get("key");
+            if(keyValue != null){
+                System.out.println("keyValue:  [" + keyValue + "]  , count:  [" + ++count + "]");
+            }else {
+                break;
+            }
+        }
 
-        Thread.sleep(1000);  //1000毫秒就是1秒
-        System.out.println("key:" + mineCache.get("key"));
         // 关闭ehcache
         ehcacheManager.close();
 
+    }
 
-        //设置缓存堆容纳元素个数(JVM内存空间)
- //.heap(1L, EntryUnit.ENTRIES)
-//设置堆外储存大小(内存存储)
-//.offheap(100L, MemoryUnit.MB)
-// 配置磁盘持久化储存(硬盘存储)
-//.disk(500L, MemoryUnit.MB, false)
+    // 缓存过期策略 是tti设置在最后的，在循环中每次访问后，这个缓存都永不过期，不会执行到break
+    @Test
+    public void ehcacheTestTTI() throws InterruptedException {
 
+
+        CacheConfiguration<String, String> TTICacheConfig = CacheConfigurationBuilder.newCacheConfigurationBuilder(
+                String.class, String.class, ResourcePoolsBuilder.newResourcePoolsBuilder().heap(300, EntryUnit.ENTRIES).offheap(30, MemoryUnit.MB).disk(60L, MemoryUnit.MB, false))
+                .withExpiry(Expirations.timeToLiveExpiration(Duration.of(1L, TimeUnit.SECONDS)))
+                .withExpiry(Expirations.timeToIdleExpiration(Duration.of(1L, TimeUnit.SECONDS)))
+                .build();
+
+        ehcacheManager.createCache("tti", TTICacheConfig);
+        Cache<String, String> mineCache = ehcacheManager.getCache("tti", String.class, String.class);
+        StringBuilder strTemp = new StringBuilder("ttiValue");
+        mineCache.put("ttiKey", strTemp.toString());
+
+
+        int count = 0;
+        while(true){
+            Thread.sleep(50);
+            String ttiValue = mineCache.get("ttiKey");
+            if(ttiValue != null){
+                System.out.println("ttiKey: [" + ttiValue + "] , count:  [" + ++count + "]");
+            }else {
+                break;
+            }
+        }
+        // 关闭ehcache
+        ehcacheManager.close();
 
     }
 
