4b443bc120c8132dc220276b8eb6c67c5dfdff1c
zhangjiandong <zhangjiandong@geotmt.com>
2019-11-06 14:03:35 +0800

    ehcache heap offHeap disk 三者之间的关系

--- src/test/java/com/geotmt/cacheprime/EhcacheTest.java
+++ src/test/java/com/geotmt/cacheprime/EhcacheTest.java
@@ -83,5 +83,102 @@ public class EhcacheTest {
 
     }
 
+    // heap  1条数据,offHeap 无  ,disk 无
+    // heap只能存放一条数据，第二条就会覆盖第一条数据，可以取出来第二条数据，取不出来第一条数据
+    // heapKey1: [heapValue测试] ,heapKey2: [测试2] ,heapKey1: [null]
+    @Test
+    public void ehcacheTestHeap(){
+
+        CacheConfiguration<String, String> heapCacheConfig = CacheConfigurationBuilder.newCacheConfigurationBuilder(
+                String.class, String.class, ResourcePoolsBuilder.newResourcePoolsBuilder().heap(1, EntryUnit.ENTRIES))
+                .withExpiry(Expirations.timeToLiveExpiration(Duration.of(1L, TimeUnit.SECONDS)))
+                .withExpiry(Expirations.timeToIdleExpiration(Duration.of(1L, TimeUnit.SECONDS)))
+                .build();
+
+        ehcacheManager.createCache("heap", heapCacheConfig);
+        Cache<String, String> mineCache = ehcacheManager.getCache("heap", String.class, String.class);
+        StringBuilder strTemp = new StringBuilder("heapValue");
+        strTemp.append("测试");
+
+        mineCache.put("heapKey1", strTemp.toString());
+        System.out.println("heapKey1: [" + mineCache.get("heapKey1") + "]");
+
+        strTemp = new StringBuilder("测试2");
+        mineCache.put("heapKey2", strTemp.toString());
+        System.out.println("heapKey2: [" +   mineCache.get("heapKey2") + "]");
+
+        System.out.println("heapKey1: [" + mineCache.get("heapKey1") + "]");
+        // 关闭ehcache
+        ehcacheManager.close();
+    }
+
+
+
+    // heap  1条数据,offHeap 1MB数据 ,disk 无 ，由于超过了offHeap储存
+    // 因此会StoreAccessException: The element with key 'heapKey1' is too large to be stored in this offheap store.
+    // 超出数据没有dick可以放置数据，因此Heapkey1 取出来的数据为null,报错后会释放掉heapKey1的offHeap储存，当数据超过1条的时候可以进去到offHeap来存储
+    // 执行结果 heapKey1: [null] ，heapKey2: [测试2]，heapKey1: [null]
+    @Test
+    public void ehcacheTestHeapAndOffheap(){
+
+        CacheConfiguration<String, String> heapCacheConfig = CacheConfigurationBuilder.newCacheConfigurationBuilder(
+                String.class, String.class, ResourcePoolsBuilder.newResourcePoolsBuilder().heap(1, EntryUnit.ENTRIES).offheap(1, MemoryUnit.MB))
+                .withExpiry(Expirations.timeToLiveExpiration(Duration.of(1L, TimeUnit.SECONDS)))
+                .withExpiry(Expirations.timeToIdleExpiration(Duration.of(1L, TimeUnit.SECONDS)))
+                .build();
+
+        ehcacheManager.createCache("heap", heapCacheConfig);
+        Cache<String, String> mineCache = ehcacheManager.getCache("heap", String.class, String.class);
+        StringBuilder strTemp = new StringBuilder("heapValue");
+        while (strTemp.toString().getBytes().length <= 1024 * 1024) {
+            strTemp.append("测试一二三四五六七八九十");
+        }
+
+        System.out.println("heapKey1 的value 大小为：" + strTemp.toString().getBytes().length + " Byte,1MB大小为:" + 1024*1024 + " Byte");
+        mineCache.put("heapKey1", strTemp.toString());
+        System.out.println("heapKey1: [" + mineCache.get("heapKey1") + "]");
+
+
+        strTemp = new StringBuilder("测试2");
+        mineCache.put("heapKey2", strTemp.toString());
+        System.out.println("heapKey2: [" +   mineCache.get("heapKey2") + "]");
+
+        // 关闭ehcache
+        ehcacheManager.close();
+    }
+
+
+    // heap  1条数据,offHeap 1MB数据 ,disk 6MB ，由于超过了offHeap最大容量,
+    // 因此会StoreAccessException: The element with key 'heapKey1' is too large to be stored in this offheap store.
+    // 但是超出的数据会有Disk来兜底，数据不会丢失，heapKey1释放掉的offHeap，当数据超过1条的时候可以进去到offHeap来存储
+    @Test
+    public void ehcacheTestDisk(){
+
+        CacheConfiguration<String, String> heapCacheConfig = CacheConfigurationBuilder.newCacheConfigurationBuilder(
+                String.class, String.class, ResourcePoolsBuilder.newResourcePoolsBuilder().heap(1, EntryUnit.ENTRIES).offheap(1, MemoryUnit.MB).disk(6L, MemoryUnit.MB, false))
+                .withExpiry(Expirations.timeToLiveExpiration(Duration.of(1L, TimeUnit.SECONDS)))
+                .withExpiry(Expirations.timeToIdleExpiration(Duration.of(1L, TimeUnit.SECONDS)))
+                .build();
+
+        ehcacheManager.createCache("heap", heapCacheConfig);
+        Cache<String, String> mineCache = ehcacheManager.getCache("heap", String.class, String.class);
+        StringBuilder strTemp = new StringBuilder("heapValue");
+        while (strTemp.toString().getBytes().length <= 1024 * 1024) {
+            strTemp.append("测试一二三四五六七八九十");
+        }
+
+        System.out.println("heapKey1 的value 大小为：" + strTemp.toString().getBytes().length + " Byte,1MB大小为:" + 1024*1024 + " Byte");
+        mineCache.put("heapKey1", strTemp.toString());
+        System.out.println("heapKey1: [" + mineCache.get("heapKey1") + "]");
+
+
+        strTemp = new StringBuilder("测试2");
+        mineCache.put("heapKey2", strTemp.toString());
+        System.out.println("heapKey2: [" +   mineCache.get("heapKey2") + "]");
+
+        // 关闭ehcache
+        ehcacheManager.close();
+    }
+
 
 }
