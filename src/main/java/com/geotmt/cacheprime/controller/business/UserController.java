24824a1c43668050ccf47849be3e8f8195cd74ed
zhangjiandong <zhangjiandong@geotmt.com>
2020-01-19 16:45:24 +0800

    用户中心

--- src/main/java/com/geotmt/cacheprime/controller/business/UserController.java
+++ src/main/java/com/geotmt/cacheprime/controller/business/UserController.java
@@ -4,7 +4,10 @@ import com.geotmt.cacheprime.utils.SessionKey;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
 
 
 @Controller
@@ -12,8 +15,18 @@ public class UserController {
 
     @GetMapping("/user")
     public String user(HttpServletRequest request, Model model){
-        model.addAttribute("username", request.getSession().getAttribute(SessionKey.SYS_USER_ACCOUNT));
+        model.addAttribute("account", request.getSession().getAttribute(SessionKey.SYS_USER_ACCOUNT));
         return "user/user";
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletRequest request, Model model){
+        Enumeration<String> attributeNames = request.getSession().getAttributeNames();
+        while (attributeNames.hasMoreElements()){
+            String attributeName = attributeNames.nextElement();
+            request.getSession().removeAttribute(attributeName);
+        }
+        return "loginPage";
     }
 
 }
